7f60e3af4b4f3f57214b047919d70db648205e25
Sergei Panfilov <flakessp@gmail.com>
2015-06-11 19:38:00 +0300

    Added basic main page template and static data

---
+++ client/views/centenarianBlocks/centenarianList.js
@@ -0,0 +1,53 @@
+var centenariansData = [
+  {
+    name: 'Roberta McCain',
+    age: '103',
+    dateBirth: '07 February 1912',
+    clickedTimes: '143'
+  },
+  {
+    name: 'Roberta McCain',
+    age: '103',
+    dateBirth: '07 February 1912',
+    clickedTimes: '143'
+  },
+  {
+    name: 'Roberta McCain',
+    age: '103',
+    dateBirth: '07 February 1912',
+    clickedTimes: '143'
+  },
+  {
+    name: 'Roberta McCain',
+    age: '103',
+    dateBirth: '07 February 1912',
+    clickedTimes: '143'
+  },
+  {
+    name: 'Roberta McCain',
+    age: '103',
+    dateBirth: '07 February 1912',
+    clickedTimes: '143'
+  },
+  {
+    name: 'Roberta McCain',
+    age: '103',
+    dateBirth: '07 February 1912',
+    clickedTimes: '143'
+  },
+  {
+    name: 'Roberta McCain',
+    age: '103',
+    dateBirth: '07 February 1912',
+    clickedTimes: '143'
+  },
+  {
+    name: 'Roberta McCain',
+    age: '103',
+    dateBirth: '07 February 1912',
+    clickedTimes: '143'
+  }
+];
+Template.centenarianList.helpers({
+  centenarians: centenariansData
+});
