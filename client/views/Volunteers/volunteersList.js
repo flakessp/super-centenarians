2be063b1524749f67e9f24d8bdd291d4b5be0894
Sergei Panfilov <flakessp@gmail.com>
2015-06-14 15:00:11 +0300

    Added volunteers template for main page -list -block

---
+++ client/views/Volunteers/volunteersList.js
@@ -0,0 +1,45 @@
+var volunteersData = [
+  {
+    name: 'Patrick Robinson',
+    rating: '1',
+    points: '245'
+  },
+  {
+    name: 'Patrick Robinson',
+    rating: '1',
+    points: '245'
+  },
+  {
+    name: 'Patrick Robinson',
+    rating: '1',
+    points: '245'
+  },
+  {
+    name: 'Patrick Robinson',
+    rating: '1',
+    points: '245'
+  },
+  {
+    name: 'Patrick Robinson',
+    rating: '1',
+    points: '245'
+  },
+  {
+    name: 'Patrick Robinson',
+    rating: '1',
+    points: '245'
+  },
+  {
+    name: 'Patrick Robinson',
+    rating: '1',
+    points: '245'
+  },
+  {
+    name: 'Patrick Robinson',
+    rating: '1',
+    points: '245'
+  }
+];
+Template.volunteersList.helpers({
+  volunteers: volunteersData
+});
