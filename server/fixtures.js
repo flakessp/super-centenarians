9a1fbcdb35e2786fc384954f2d734a14f0023ebb
Sergei Panfilov <flakessp@gmail.com>
2015-06-15 19:28:37 +0300

    Added HTML/CSS + static media for Centenarian Page template

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,9 +1,12 @@
 if (Centenarians.find().count() === 0) {
   Centenarians.insert({
-    name: 'Roberta McCain',
+    name: 'Robert McCain',
     age: '103',
     dateBirth: '07 February 1912',
-    clickedTimes: '143'
+    clickedTimes: '143',
+    state: 'Inkster Michigan',
+    ethnicity: 'African American',
+    lifestyle: 'Sport'
   });
 
   Centenarians.insert({
